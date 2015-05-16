bde521123158396b83c7ad6953d111c390cf62d0
Mbuyamba Mutombo Beatrice <mukanda.kengwe@gmail.com>
2015-05-16 04:57:01 +0200

    Sud Kivu
    Sud Kivu

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="css/style.css" type="text/css" />
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+
+		<script type="text/javascript"
+      			src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD_M34eZg0zFUjwYltQ51lgZg3RqEkfqKc" sensor=true>
+    	</script>
+
+		<title>SUD KIVU</title>
+	</head>
+
+	<body>
+
+		<div id="banniere">
+			<h2>Sud Kivu</h2>	
+		</div>
+
+
+		<div id="news2">
+		</div>
+
+		<div id="map-canvas">
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2039432.2183337375!2d28.031442849999998!3d-3.2841000499999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x19ea2ec21f044c99%3A0x95fd4a4a6e234625!2sSud-Kivu%2C+R%C3%A9publique+d%C3%A9mocratique+du+Congo!5e0!3m2!1sfr!2s!4v1431739472356" width="865" height="500" frameborder="0" style="border:0"></iframe>
+		</div>
+
+		<div id="pied">
+		</div>
+	</body>
+
+
+	<script>
+/*
+		var map;
+		
+		function initialise(location) 
+		{
+			console.log(location);
+			var mapOptions = {
+  			center: new google.maps.LatLng(-3.2841,28.0314428,8),
+  			zoom: 8
+			};
+
+			map = new google.maps.Map(document.getElementById("map-canvas"),
+				mapOptions);
+		}
+
+		$(document).ready(function()
+		{
+			navigator.geolocation.getCurrentPosition(initialise);
+		});
+
+*/
+	</script>
+</html>
